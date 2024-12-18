2e80a8b8bb67a39b4587f03882711eee273a5646
Joshua <mrjoshuabanda@gmail.com>
2024-12-18 18:38:34 +0500

    поместил все hash map в класс product.

--- src/main/java/org/Baraxolkabot/MassegeKeyboard/Product.java
+++ src/main/java/org/Baraxolkabot/MassegeKeyboard/Product.java
@@ -1,6 +1,10 @@
 package org.Baraxolkabot.MassegeKeyboard;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
 import org.Baraxolkabot.Category.Category;
 
 public class Product {
@@ -12,12 +16,20 @@ public class Product {
     private final Category categoryName;
 
 
+    public static final Map<Long, String> addingState = new HashMap<>();
+    public static final Map<Long, String> priceState = new HashMap<>();
+    public static final Map<Long, String> descriptionState = new HashMap<>();
+    public static final Map<Long, String> phoneState = new HashMap<>();
+    public static List<Category> categories = new ArrayList<>();
+    public static final Map<Long, Category> categoryState = new HashMap<>();
+
+
     public Product(String name,Category categoryName, BigDecimal price, String description, String telegramHandle, String photoId) {
         this.name = name;
         this.categoryName = categoryName;
         this.price = price;
         this.description = description;
-        this.telegramHandle = formatTelegramHandle(telegramHandle);
+        this.telegramHandle = telegramHandle;
         this.photoId = photoId;
     }
 
@@ -43,13 +55,6 @@ public class Product {
 
     public Category getCategory() {
         return categoryName;
-    }
-
-    private String formatTelegramHandle(String handle) {
-        if (handle == null) {
-        return "@unknown";
-        }
-        return handle.startsWith("@") ? handle : "@" + handle;
     }
 
 }
