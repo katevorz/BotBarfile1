2a3e46419c8838754b721219600418b4619426f0
kvorz <kvorzopova@gmail.com>
2024-12-19 01:36:20 +0500

    Тут теперь клавиатура

---
+++ src/main/java/org/Baraxolkabot/MassegeKeyboard/KeyboardUtils.java
@@ -0,0 +1,44 @@
+package org.Baraxolkabot.MassegeKeyboard;
+
+import org.Baraxolkabot.Category.Category;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KeyboardUtils {
+
+    public static ReplyKeyboardMarkup createMainKeyboard() {
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+        List<KeyboardRow> keyboard = new ArrayList<>();
+
+        KeyboardRow row1 = new KeyboardRow();
+        row1.add("Добавить товар");
+        row1.add("Показать все товары");
+
+        KeyboardRow row2 = new KeyboardRow();
+        row2.add("Поиск товара");
+        row2.add("Удалить товар");
+
+        keyboard.add(row1);
+        keyboard.add(row2);
+        keyboardMarkup.setKeyboard(keyboard);
+
+        return keyboardMarkup;
+    }
+
+    public static ReplyKeyboardMarkup createCategoryKeyboard() {
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+        List<KeyboardRow> keyboard = new ArrayList<>();
+
+        for (Category category : Product.categories) {
+            KeyboardRow row = new KeyboardRow();
+            row.add(category.getName());
+            keyboard.add(row);
+        }
+
+        keyboardMarkup.setKeyboard(keyboard);
+        return keyboardMarkup;
+    }
+}
